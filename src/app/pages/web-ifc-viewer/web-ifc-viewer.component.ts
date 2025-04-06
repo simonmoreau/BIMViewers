3f1b3bf1f010e87af9678f81fb13d1891a1c7af1
Simon Moreau <simon@bim42.com>
2025-04-06 13:47:57 +0200

    Add ifc viewer page

---
+++ src/app/pages/web-ifc-viewer/web-ifc-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-ifc-viewer',
+  standalone: false,
+  templateUrl: './web-ifc-viewer.component.html',
+  styleUrl: './web-ifc-viewer.component.scss'
+})
+export class WebIfcViewerComponent {
+
+}
