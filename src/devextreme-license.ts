018ca806336c7674f69c62fc823b6397b33524ec
Simon Moreau <simon@bim42.com>
2025-04-06 20:43:29 +0200

    licence

---
+++ src/devextreme-license.ts
@@ -0,0 +1,8 @@
+// This file allows you to configure ESLint according to your project's needs, so that you
+// can control the strictness of the linter, the plugins to use, and more.
+
+// For more information about configuring ESLint, visit https://eslint.org/docs/user-guide/configuring/
+
+export const licenseKey = 'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImVmMjQ4OWYyLTA4Y2YtNGEzNi05Y2RiLTQwZjM2NTkzOTY4MSIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjQyCn0=.e1gUZOrMVhXGNoiDdEdo9KFqJIX2Kjt1MIcBDiWM2ZkxM/rHjZT0JJtg0Pm5lFY7PhessnPu6t4kdGQIMv2KfSiKX1OlL13ER6dp4zsijhiJLiEJnki7Gxpn97/1/+s/i2J2pA==';
+
+
