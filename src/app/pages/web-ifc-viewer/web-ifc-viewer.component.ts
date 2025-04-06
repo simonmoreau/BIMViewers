7055d9cc7e2ddbf0972594d4979960815bf89e6f
Simon Moreau <simon@bim42.com>
2025-04-06 14:42:13 +0200

    create viewer

--- src/app/pages/web-ifc-viewer/web-ifc-viewer.component.ts
+++ src/app/pages/web-ifc-viewer/web-ifc-viewer.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import * as BUI from "@thatopen/components/";
+import { Component, OnInit } from '@angular/core';
+import * as OBC from '@thatopen/components/';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-web-ifc-viewer',
   standalone: false,
   templateUrl: './web-ifc-viewer.component.html',
-  styleUrl: './web-ifc-viewer.component.scss'
+  styleUrl: './web-ifc-viewer.component.scss',
 })
-export class WebIfcViewerComponent {
+export class WebIfcViewerComponent implements OnInit {
+  ngOnInit(): void {
+    const container = document.getElementById('container')!;
+    const components = new OBC.Components();
+    const worlds = components.get(OBC.Worlds);
 
+    const world = worlds.create<
+      OBC.SimpleScene,
+      OBC.SimpleCamera,
+      OBC.SimpleRenderer
+    >();
+
+    world.scene = new OBC.SimpleScene(components);
+    world.renderer = new OBC.SimpleRenderer(components, container);
+    world.camera = new OBC.SimpleCamera(components);
+
+    components.init();
+    world.scene.setup();
+
+    const material = new THREE.MeshLambertMaterial({ color: "#6528D7" });
+    const geometry = new THREE.BoxGeometry();
+    const cube = new THREE.Mesh(geometry, material);
+    world.scene.three.add(cube);
+  }
 }
