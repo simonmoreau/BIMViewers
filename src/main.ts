12d0a44d1332fe161ff38254bcbbc60e870bd8a4
Simon Moreau <simon@bim42.com>
2025-04-06 13:22:51 +0200

    Add license

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import themes from 'devextreme/ui/themes';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import config from 'devextreme/core/config'; 
+import { licenseKey } from './devextreme-license'; 
 
 themes.initialized(() => {
+  config({ licenseKey });
   platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
